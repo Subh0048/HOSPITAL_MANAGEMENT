ef1a2d0733de0c21038c4aebdea55477c326341b
Subh0048 <subhamkumarsahu99@gmail.com>
2024-06-01 13:47:38 +0530

    added env.sample file

--- utils/JwtTOken.js
+++ utils/JwtTOken.js
@@ -1,4 +1,8 @@
 export const generateToken=(user,message,statusCode,res)=>{
-    
+    const token = user.generatejsonWebToken()
+    const cookiename = user.role === "Admin"? "adminToken":"patientToken"
+    res.status(statusCode).cookie(cookieName,token,{
+        expires:new Date(Date.now()+process.env.JWT_EXPIRE)
+    })
 
 }
